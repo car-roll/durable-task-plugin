96d09d1ad56c679fe3a08e78e635b8493f013309
Carroll Chiou <cchiou@cloudbees.com>
2019-08-05 12:46:53 -0700

    make rebuild script more generic

--- src/main/go/org/jenkinsci/plugins/durabletask/rebuild.sh
+++ src/main/go/org/jenkinsci/plugins/durabletask/rebuild.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
 # Convenience script to aid in quick rebuilding of golang binaries
 set -x
+TOP_LEVEL=$(git rev-parse --show-toplevel)
 NAME="durable_task_monitor"
-RESFOLDER="${HOME}/code/github/durable-task-plugin/src/main/resources/org/jenkinsci/plugins/durabletask"
+RESFOLDER="${TOP_LEVEL}/src/main/resources/org/jenkinsci/plugins/durabletask"
 rm -rf ${RESFOLDER}/${NAME}_*
 env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -a -o ${NAME}_DARWIN64
 env CGO_ENABLED=0 GOOS=darwin GOARCH=386 go build -a -o ${NAME}_DARWIN32
