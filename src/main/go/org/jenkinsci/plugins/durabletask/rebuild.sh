e432b34a172d051a13921b87f4eb73da0947f8db
Carroll Chiou <cchiou@cloudbees.com>
2019-08-06 23:20:17 -0700

    Add system property to force shell wrapper refactor to make code path between shell and binary more similar address various review comments refactor binary name to all lowercase

--- src/main/go/org/jenkinsci/plugins/durabletask/rebuild.sh
+++ src/main/go/org/jenkinsci/plugins/durabletask/rebuild.sh
@@ -5,8 +5,8 @@ TOP_LEVEL=$(git rev-parse --show-toplevel)
 NAME="durable_task_monitor"
 RESFOLDER="${TOP_LEVEL}/src/main/resources/org/jenkinsci/plugins/durabletask"
 rm -rf ${RESFOLDER}/${NAME}_*
-env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -a -o ${NAME}_DARWIN64
-env CGO_ENABLED=0 GOOS=darwin GOARCH=386 go build -a -o ${NAME}_DARWIN32
-env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o ${NAME}_UNIX64
-env CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -a -o ${NAME}_UNIX32
+env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -a -o ${NAME}_darwin64
+env CGO_ENABLED=0 GOOS=darwin GOARCH=386 go build -a -o ${NAME}_darwin32
+env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o ${NAME}_unix64
+env CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -a -o ${NAME}_unix32
 mv ${NAME}_* ${RESFOLDER}/.
